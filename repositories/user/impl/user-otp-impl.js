abb80d8e05a72cd4d1d11a3f7ec9ce6ba5f5e0be
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-10-19 14:32:04 +0600

    verify otp method added

--- repositories/user/impl/user-otp-impl.js
+++ repositories/user/impl/user-otp-impl.js
@@ -1,7 +1,26 @@
-const Otp = require('../../../schemas/otp')
+const Otp = require('../../../schemas/otp');
+const User = require('../../../schemas/user');
+const bcrypt = require('bcrypt');
+
 class UserOtpImpl {
     static async insertOtp(email) {
         return await Otp.insertOtp(email);
+    }
+
+    static async verifyOtp(params) {
+        const record = await Otp.findOne({email: params.email});
+        if (!record) {
+            throw new Error(`Otp already expired`);
+        } else {
+            if (params.otp === record.otp) {
+                await User.create({
+                    email: params.email,
+                    password: await bcrypt.hash(params.password, 10)
+                });
+            } else {
+                throw new Error(`Wrong otp`);
+            }
+        }
     }
 }
 
