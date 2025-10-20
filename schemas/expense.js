2f1073ee8701760a782cd5ffa32560539e7f6540
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-10-20 16:00:31 +0600

    expense schema

---
+++ schemas/expense.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const expenseSchema = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    },
+    category_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
+        index: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    account: {
+        type: String,
+        required: true,
+        index: true
+    },
+    note: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Expense', expenseSchema);
