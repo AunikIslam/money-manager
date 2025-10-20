02b776a4b1001837f876452e6f8783b4e6e7377f
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-10-20 15:57:53 +0600

    auth middleware added

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 const path = require("path");
-
-
 const endpoints = require('./config/endpoints');
 const baseUrls = require('./config/base-urls');
 const utilFunctions = require('./utils/util-functions')
@@ -17,10 +15,13 @@ const checkForWhiteListUrl = require('./utils/white-list-urls');
 const ApiResponse = require("./models/api-response");
 const browserPool = require('./config/browser-pool');
 const authRoutes = require('./routes/auth-routes');
+const authMiddleware = require('./middlewares/auth-middleware');
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
+app.use('/other-routers', authMiddleware.verifyToken);
 
 setupSwagger(app);
 
