c31a583c30a2666d7bebe0de5be34c35aeea968d
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-10-20 15:58:41 +0600

    auth middleware implementation

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const ApiResponse = require('../models/api-response')
+
+exports.verifyToken = (req, res, next) => {
+    const header = req.headers.authorization;
+    if (!header) {
+        return res.status(401).json(new ApiResponse.Error(['Authorization header not available'], 401));
+    }
+    const token = header.split(' ')[1];
+    if (!token) {
+        return res.status(401).json(new ApiResponse.Error(['Token not available'], 401));
+    }
+    try {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+        next()
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json(new ApiResponse.Error(['Token expired'], 401));
+        } else {
+            return res.status(401).json(new ApiResponse.Error(['Invalid token'], 401));
+        }
+    }
+}
