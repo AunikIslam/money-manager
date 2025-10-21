b0244fe9eee050c57dbb5c4a0e191771292d08f0
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-10-21 14:20:00 +0600

    1. refactoring 2. getToken implemented

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,28 +1,28 @@
-const userOtpImpl = require('../repositories/auth/impl/user-otp-impl');
-const userEmailImpl = require('../repositories/auth/impl/user-email-impl');
+const UserOtpImpl = require('../repositories/auth/impl/user-otp-impl');
+const UserEmailImpl = require('../repositories/auth/impl/user-email-impl');
+const UserTokenImpl = require('../repositories/auth/impl/user-token-impl');
 const ApiResponse = require("../models/api-response");
 const User = require('../schemas/user');
 
 
-exports.insertAndSendOtp = async (req,res) => {
+exports.insertAndSendOtp = async (req, res) => {
     const duplicateUser = await User.findOne({email: req.body.email.toLowerCase()});
     if (duplicateUser) {
         return res.status(400).json(new ApiResponse.Error(['User already exists'], 400));
     }
     try {
-        const response  = await userOtpImpl.insertOtp(req.body.email);
-        await userEmailImpl.sendSignUpOtpEmail(response.email.toLowerCase(), response.otp);
+        const response = await UserOtpImpl.insertOtp(req.body.email);
+        await UserEmailImpl.sendSignUpOtpEmail(response.email.toLowerCase(), response.otp);
         return res.status(200).json(new ApiResponse.Success('An otp has been sent to your email address.'));
-    }
-    catch (err) {
+    } catch (err) {
 
     }
 }
 
-exports.verifyOtp = async (req,res) => {
+exports.verifyOtp = async (req, res) => {
     try {
         const {email, password, otp} = req.body;
-        const token = await userOtpImpl.verifyOtp({email, password, otp});
+        const token = await UserOtpImpl.verifyOtp({email, password, otp});
         return res.status(200).json(new ApiResponse.Success({
             access_token: token.access_token,
             refresh_token: token.refresh_token,
@@ -34,7 +34,15 @@ exports.verifyOtp = async (req,res) => {
     }
 }
 
-exports.login = async (req,res) => {
+exports.getToken = async (req, res) => {
+    const token = await UserTokenImpl.getToken(req.body.email);
+    return res.status(200).json(new ApiResponse.Success({
+        access_token: token.access_token,
+        refresh_token: token.refresh_token,
+    }));
+}
+
+exports.login = async (req, res) => {
     try {
 
     } catch (error) {
