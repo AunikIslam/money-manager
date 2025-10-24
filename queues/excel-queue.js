c3543d59417e886eccb220518fb10da397492df1
AunikIslam <aunikislam172@gmail.com>
2025-10-24 19:38:23 +0600

    excel queue file added

---
+++ queues/excel-queue.js
@@ -0,0 +1,9 @@
+const {Queue: ExcelQueue} = require('bullmq');
+const IORedis = require('ioredis');
+
+const connection = new IORedis({
+    maxRetriesPerRequest: null
+});
+
+exports.excelQueue = new ExcelQueue('excel-processing', {connection});
+
