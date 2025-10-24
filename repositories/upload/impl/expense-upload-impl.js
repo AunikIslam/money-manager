68137c56cd8c23c4d5de1c01ad4c65fd420c4af9
AunikIslam <aunikislam172@gmail.com>
2025-10-24 19:40:34 +0600

    expense upload impl

---
+++ repositories/upload/impl/expense-upload-impl.js
@@ -0,0 +1,44 @@
+const multer = require('multer');
+const rootDir = require('../../../utils/path');
+const path = require('path');
+const fs = require('fs');
+const sessionContextService = require('../../../services/session-context-service')
+
+class ExpenseUploadImpl {
+    static prepareMulter() {
+        const uploadDir = path.join(rootDir, 'uploads', 'expenses');
+        if (!fs.existsSync(uploadDir)) {
+            // recursive will create an uploads directory if it does not exist
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+
+        const storage = multer.diskStorage({
+            destination: (req, file, cb) => {
+                cb(null, uploadDir);
+            },
+            filename: (req, file, cb) => {
+                const name = `${sessionContextService.getUserId()}_expense.xlsx`;
+                cb(null, name);
+            }
+        });
+
+        const fileFilter = (req, file, cb) => {
+            if (
+                file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
+                file.mimetype === 'application/vnd.ms-excel'
+            ) {
+                cb(null, true);
+            } else {
+                cb(new Error('Only Excel files are allowed!'), false);
+            }
+        };
+
+        return multer({storage, fileFilter});
+    }
+
+    static performMulterUpload(req, res) {
+
+    }
+}
+
+module.exports = ExpenseUploadImpl;
