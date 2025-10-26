34388ca58e85ad527e2c70bb73efc5b11a03e514
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-10-26 12:47:53 +0600

    invalid data preparation updated

--- repositories/upload/impl/expense-upload-impl.js
+++ repositories/upload/impl/expense-upload-impl.js
@@ -2,14 +2,15 @@ const multer = require('multer');
 const rootDir = require('../../../utils/path');
 const path = require('path');
 const fs = require('fs');
-const sessionContextService = require('../../../services/session-context-service')
+const sessionContextService = require('../../../services/session-context-service');
+const ErrorEntry = require('../../../schemas/expense-error-entry');
 
 class ExpenseUploadImpl {
     static prepareMulter() {
         const uploadDir = path.join(rootDir, 'uploads', 'expenses');
         if (!fs.existsSync(uploadDir)) {
             // recursive will create an uploads directory if it does not exist
-            fs.mkdirSync(uploadDir, { recursive: true });
+            fs.mkdirSync(uploadDir, {recursive: true});
         }
 
         const storage = multer.diskStorage({
@@ -41,12 +42,40 @@ class ExpenseUploadImpl {
         const invalidEntries = [];
 
         data.forEach(pData => {
-           if (!data.category) {
-               invalidEntries.push({
+            let error = null;
+            if (!pData.category) {
+                error = 'Category not available'
+            }
+            else if (!pData.amount) {
+                error = 'Amount not available'
+            }
+            else if (!pData.account) {
+                error = 'Account not available'
+            }
+            else if (!pData.date) {
+                error = 'Date not available'
+            }
+            else if (!pData.time) {
+                error = 'Time not available'
+            }
+            else if (!pData.transaction_type) {
+                error = 'Transaction type not available'
+            }
+            const errorEntry =
+                new ErrorEntry()
+                    .setCategory(data.category)
+                    .setAmount(data.amount)
+                    .setAccount(data.account)
+                    .setNote(data.note)
+                    .setDescription(data.description)
+                    .setDate(data.date)
+                    .setTime(data.time)
+                    .setTransactionType(data.transaction_type)
+                    .setError(error)
 
-               })
-           }
+            invalidEntries.push(errorEntry);
         });
+        console.log(invalidEntries);
     }
 }
 
